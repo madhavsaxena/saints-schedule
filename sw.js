9d10edbe65dfff655b17d0753159230d35c1841f
Madhav Saxena <madhav@namassolutions.com>
2018-11-09 10:39:32 -0600

    Fixed sw for offline access

--- sw.js
+++ sw.js
@@ -1,4 +1,36 @@
-/** An empty service worker! */
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('saints-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/bars.html',
+        '/main.html',
+        '/manifest.json',
+        '/news.html',
+        '/next.html',
+        '/previous.html',
+        '/sw.js',
+        '/json/bars.json',
+        '/json/nextgames.json',
+        '/json/previousgames.json',
+        '/assets/arrow-back.png',
+        '/assets/arrow-forward.png',
+        '/assets/bar.png',
+        '/assets/bengals.png',
+        '/assets/home.png',
+        '/assets/main.css',
+        '/assets/news.png',
+        '/assets/rams.png',
+      ]);
+    })
+  );
+});
+
 self.addEventListener('fetch', function(event) {
-  /** An empty fetch handler! */
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
 });
